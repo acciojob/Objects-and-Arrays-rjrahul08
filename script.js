bd7317bdd6b40cc9964b54d2d684d23eaa35c88c
Rahul Prasad Shaw <61372250+rjrahul08@users.noreply.github.com>
2023-07-19 12:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create copies of the array and object
+const team = players;
+const team1 = [...players];
+const cap1 = { ...person };
 
+// Assign the variables to the global object (window)
 window.players = players;
 window.person = person;
 window.team = team;
